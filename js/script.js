79244f0cc7d77b112f98513eaf2ba4cd8e89213b
richbruzzese <85512661+richbruzzese@users.noreply.github.com>
2021-06-08 13:56:45 -0400

    Minor formatting changes / removed initial script i created

--- js/script.js
+++ js/script.js
@@ -10,115 +10,119 @@ project 1 - A Random Quote Generator
 /*** 
 Array of objects to store quotes. Properties to store quote, source, along with  citation and year(where applicable)
 ***/
-let quotes=[{
-  quote:'The journey of a thousand miles begins with one step.',
-  source:'Lao Tzu',
-  citation:'Dao De Jing'
-},
-{
-  quote:'That which does not kill us makes us stronger',
-  source:'Friedrich Nietzsche',
-  citation:'Twilight of the Idols',
-  year:'1888'
-},
-{
-  quote:'Life is what happens when you’re busy making other plans.',
-  source:'John Lennon',
-  citation:'Beautiful Boy',
-  year: 1981
-},
-{
-  quote:'The greatest glory in living lies not in never falling, but in rising every time we fall.',
-  source:'Nelson Mandela'
-},{
-  quote:'The way to get started is to quit talking and begin doing',
-  source:'Walt Disney'
-},{
-  quote:'Don\'t judge each day by the harvest you reap but by the seeds that you plant.',
-  source:'Robert Louis Stevenson'
-},
-{
-  quote:'"Tell me and I forget. Teach me and I remember. Involve me and I learn.',
-  source:'Benjamin Franklin',
-},
-{
-  quote:'It is during our darkest moments that we must focus to see the light.',
-  source:'Aristotle'
-},
-{
-  quote:'Whoever is happy will make others happy too.',
-  source:'Anne Frank'
-},
-{
-  quote:'Life is either a daring adventure or nothing at all.',
-  source:'Hellen Keller'
-},
-{
-  quote:'You only live once, but if you do it right, once is enough.',
-  source:'Mae West'
-},
-{
-  quote:'Some infinities are greater than others',
-  source:'John Green',
-  citation: 'The Fault in Our Stars',
-  year: 2012
-},
-{
-  quote:'Grief doesn\'t change you, it reveals you',
-  source:'John Green'
-},
-{
-  quote:'A short pencil is better than a long memory',
-  source:'Unknown'
-},
-{
-  quote:'Count your blessing, and not your problems',
-  source:'Unkown'
-},
-{
-  quote:'A reader lives a thousand lives before he dies. The man who never reads only lives once',
-  source:'George R R Martin'
-},
-{
-  quote:'Anything is possible. Anything can be',
-  source:'Shel Silverstein'
-},
-{
-  quote:'When you look at someone through rose-colored glasses, all the red flags just look like flags',
-  source:'Wanda Pierce',
-  citation: 'Bojack Horseman',
-  year: 2015
-}
+
+let quotes=[
+    {
+    quote:'The journey of a thousand miles begins with one step.',
+    source:'Lao Tzu',
+    citation:'Dao De Jing'
+  },
+  {
+    quote:'That which does not kill us makes us stronger',
+    source:'Friedrich Nietzsche',
+    citation:'Twilight of the Idols',
+    year:'1888'
+  },
+  {
+    quote:'Life is what happens when you’re busy making other plans.',
+    source:'John Lennon',
+    citation:'Beautiful Boy',
+    year: 1981
+  },
+  {
+    quote:'The greatest glory in living lies not in never falling, but in rising every time we fall.',
+    source:'Nelson Mandela'
+  },{
+    quote:'The way to get started is to quit talking and begin doing',
+    source:'Walt Disney'
+  },{
+    quote:'Don\'t judge each day by the harvest you reap but by the seeds that you plant.',
+    source:'Robert Louis Stevenson'
+  },
+  {
+    quote:'"Tell me and I forget. Teach me and I remember. Involve me and I learn.',
+    source:'Benjamin Franklin',
+  },
+  {
+    quote:'It is during our darkest moments that we must focus to see the light.',
+    source:'Aristotle'
+  },
+  {
+    quote:'Whoever is happy will make others happy too.',
+    source:'Anne Frank'
+  },
+  {
+    quote:'Life is either a daring adventure or nothing at all.',
+    source:'Hellen Keller'
+  },
+  {
+    quote:'You only live once, but if you do it right, once is enough.',
+    source:'Mae West'
+  },
+  {
+    quote:'Some infinities are greater than others',
+    source:'John Green',
+    citation: 'The Fault in Our Stars',
+    year: 2012,
+    link: 'https://www.amazon.com/Fault-Our-Stars-John-Green/dp/014242417X'
+  },
+  {
+    quote:'Grief doesn\'t change you, it reveals you',
+    source:'John Green'
+  },
+  {
+    quote:'A short pencil is better than a long memory',
+    source:'Unknown'
+  },
+  {
+    quote:'Count your blessing, and not your problems',
+    source:'Unkown'
+  },
+  {
+    quote:'A reader lives a thousand lives before he dies. The man who never reads only lives once',
+    source:'George R R Martin'
+  },
+  {
+    quote:'Anything is possible. Anything can be',
+    source:'Shel Silverstein'
+  },
+  {
+    quote:'When you look at someone through rose-colored glasses, all the red flags just look like flags',
+    source:'Wanda Pierce',
+    citation: 'Bojack Horseman',
+    year: 2015,
+    link:'https://www.netflix.com/title/70300800'
+  }
 
 ]
-
 
 /***
  Function will generate a random number based on length of the array.
  After generating the number assigning it to the index position and 
  returning the quote from that index value
 ***/
+
 function getRandomQuote (){
   let randomNumber = Math.floor(Math.random() * quotes.length )
   let randomQuote = quotes[randomNumber]
   return randomQuote
 }
 
-
 /***
  Function will call the getRandomQuote function, create an html string and break
  out the properties to specific parts of the page.
 ***/
+
 function printQuote( ){
   let randQuote=getRandomQuote();
   let html = `<p class= "quote">` + randQuote.quote + '</p>' 
   + `<p class= "source">` + randQuote.source  
-  if(randQuote.citation){
-    html += `<span class="citation">` + randQuote.citation + `</span>`;
-    if(randQuote.year){
-      html += `<span class="year">` + randQuote.year + `</span>`;
+    if(randQuote.citation){
+      html += `<span class="citation">` + randQuote.citation + `</span>`;
+        if(randQuote.year){
+          html += `<span class="year">` + randQuote.year + `</span>`;
+        }
     }
-  }
 
   html += `</p>`;
   return document.getElementById('quote-box').innerHTML = html;
